c1b5abe1156684610ec35330ce56f1be295e7f85
pecete <javier.pecete@innovation4security.com>
2017-11-12 15:12:36 +0100

    Fixes

--- AWSBot.py
+++ AWSBot.py
@@ -25,6 +25,7 @@ class botCli(object):
         self.get_url(url)
 
     def get_json_from_url(self,url):
+        js = {}
         try:
             content = self.get_url(url)
             js = json.loads(content)
@@ -38,13 +39,13 @@ class botCli(object):
         return js
 
     def get_last_chat_id_and_text(self,updates):
-        num_updates = len(updates["result"])
-        last_update = num_updates - 1
-        text = updates["result"][last_update]["message"]["text"]
-        chat_id = updates["result"][last_update]["message"]["chat"]["id"]
-        m_id = updates["result"][last_update]["update_id"]
+            num_updates = len(updates["result"])
+            last_update = num_updates - 1
+            text = updates["result"][last_update]["message"]["text"]
+            chat_id = updates["result"][last_update]["message"]["chat"]["id"]
+            m_id = updates["result"][last_update]["update_id"]
+            return (chat_id,text,m_id)
 
-        return (chat_id,text,m_id)
 
     def execute_analysis_aws(self,chat_id,args):
 
@@ -75,31 +76,45 @@ class botCli(object):
             self.send_message(line.decode('utf-8'),chat_id)
 
     def get_initID(self):
-        _, _, init_update = self.get_last_chat_id_and_text(self.get_updates())
+        init_update = 0
+        if(len(self.get_json_from_url(self.url + "getUpdates?")["result"])==0):
+           init_update = 0
+        else:
+            _, _, init_update = self.get_last_chat_id_and_text(self.get_updates())
+
         return init_update
 
     def run(self,chat_id):
         last_id = self.get_initID()
         logging.info("[*][*][*][*][*] Running Telegram Server Bot ... [*][*][*][*][*]")
         while True:
-            user_id, msg, current_id = self.get_last_chat_id_and_text(self.get_updates())
-            if user_id in chat_id and current_id > last_id:
-                last_id = current_id
-                if msg.split(' ', 1)[0] == "/ScanAWS":
-                    if len(msg.split())==1:
-                        self.send_message(
-                            "Unable to locate credentials. You can configure credentials by running \"aws configure\"",
-                            user_id)
-                    else:
-                        self.execute_analysis_aws(user_id, msg.split())
-                        logging.info("Request of %i User to execute ScanAWS",user_id)
-                if msg.split(' ', 1)[0] == "/Nmap":
-                    self.execute_nmap(user_id,msg.split())
-                    logging.info("Request of %i User to execute Nmap", user_id)
+            try:
+                user_id, msg, current_id = self.get_last_chat_id_and_text(self.get_updates())
 
-                else:
-                    self.send_message("To scan your AWS Account use /ScanAWS -p \"profileAWS\", to scan another network could use /Nmap with {nmap args}",user_id)
-                    logging.info("Request Failed of %i User without corrects parameters", user_id)
+                if user_id in chat_id and current_id > last_id:
+                    last_id = current_id
+                    if msg.split(' ', 1)[0] == "/ScanAWS":
+                        if len(msg.split())==1:
+                            self.send_message(
+                                "Unable to locate credentials. You can configure credentials by running \"aws configure\"",
+                                user_id)
+                        else:
+                            self.execute_analysis_aws(user_id, msg.split())
+                            logging.info("Request of %i User to execute ScanAWS",user_id)
+                    if msg.split(' ', 1)[0] == "/Nmap":
+                        self.execute_nmap(user_id,msg.split())
+                        logging.info("Request of %i User to execute Nmap", user_id)
+
+                    else:
+                        self.send_message("To scan your AWS Account use /ScanAWS -p \"profileAWS\", to scan another network could use /Nmap with {nmap args}",user_id)
+                        logging.info("Request Failed of %i User without corrects parameters", user_id)
+
+            except IndexError:
+                logging.error("Problem parsing JSON")
+            except TypeError:
+                logging.error("Problem parsing JSON")
+            except KeyError:
+                logging.error("Problem parsing JSON")
 
 if __name__ == '__main__':
 
@@ -123,7 +138,6 @@ if __name__ == '__main__':
 
     #Create de cli
     botcli = botCli(args.path,url,args.token)
-
     #Your ID's user
     botcli.run(args.users)
 
